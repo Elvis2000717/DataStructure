c818eee3dee1a29425169038bf4f9f9938ff7d1d
Elvis2000717 <1147445979@qq.com>
2021-01-29 15:09:19 +0800

    2021.1.29

---
+++ middleNode1.c
@@ -0,0 +1,10 @@
+struct ListNode* middleNode(struct ListNode* head){
+	struct ListNode* fast, *slow;
+	fast = slow = head;
+	while (fast&& fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+}
