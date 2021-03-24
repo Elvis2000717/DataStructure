c0c214ac59088cc7dbf8586c7e701480c0135326
Elvis2000717 <1147445979@qq.com>
2021-03-24 22:38:33 +0800

    2021.3.24 冒泡排序

---
+++ 冒泡排序.c
@@ -0,0 +1,21 @@
+void BubbleSort(int* a, int n)
+{
+	int end = n;
+	while (end > 0)
+	{
+		int exchange = 0;
+		for (int i = 1; i < end; i++)
+		{
+			if (a[i - 1]>a[i])
+			{
+				Swap(&a[i - 1], &a[i]);
+				exchange = 1;
+			}
+		}
+		if (exchange == 0)
+		{
+			break;
+		}
+		end--;
+	}
+}
