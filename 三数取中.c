a286491f753fbfd593b404f14b348e92925ceffe
Elvis2000717 <1147445979@qq.com>
2021-03-27 22:54:48 +0800

    2021.3.27

---
+++ 三数取中.c
@@ -0,0 +1,34 @@
+int GetMidIndex(int* a, int left, int right)
+{
+	int mid = (left + right) >> 1;
+	if (a[left] < a[mid])
+	{
+		if (a[mid] < a[right])
+		{
+			return mid;
+		}
+		else if (a[left] > a[right])
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+	else //a[left] > a[mid]
+	{
+		if (a[mid] > a[right])
+		{
+			return mid;
+		}
+		else if (a[left] > a[right])
+		{
+			return right;
+		}
+		else
+		{
+			return left;
+		}
+	}
+}
